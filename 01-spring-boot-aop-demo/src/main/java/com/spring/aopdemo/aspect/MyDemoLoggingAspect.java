d091efe2a44dd23f28b3f8ec6d4f12cc8a85b859
Rishib#321 <rishitab2209@gmail.com>
2025-02-03 13:31:39 +0530

    spring aop

--- 01-spring-boot-aop-demo/src/main/java/com/spring/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 01-spring-boot-aop-demo/src/main/java/com/spring/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -12,6 +12,7 @@ public class MyDemoLoggingAspect {
 
     // starting with an @Before Advice
 
+    /*
     // this will be applied only on the addAccount method...
     @Before("execution(public void addAccount())")
   // @Before("execution(public void updateAccount())")
@@ -21,6 +22,17 @@ public class MyDemoLoggingAspect {
 
     //   System.out.println("\n==> Executing @Before advice on method");
     }
+     */
 
+
+    // if we want to apply @Before Advice only on specific method (for which we have 2 methods with the same name)
+    // will match on interface or class that implements the interface
+    // package name + class/interface name
+    // advice will not be applied to membership interface(here)
+    @Before("execution(public void com.spring.aopdemo.dao.AccountDAO.addAccount())")
+    public void beforeAddAccountAdvice() {
+
+        System.out.println("\n==> Executing @Before advice on method");
+    }
 
 }
