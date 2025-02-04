608c8dc17a83d1f246b77aefcf048e66c31cbae0
Rishib#321 <rishitab2209@gmail.com>
2025-02-04 09:31:15 +0530

    spring aop pointcut-declarations, ordering aspects

---
+++ 03-spring-boot-aop-Ordering-Aspects/src/main/java/com/spring/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -0,0 +1,11 @@
+package com.spring.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyApiAnalyticsAspect {
+
+
+}
