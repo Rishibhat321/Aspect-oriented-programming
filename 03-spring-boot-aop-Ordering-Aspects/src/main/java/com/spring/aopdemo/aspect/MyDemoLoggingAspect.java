aea6f4771f2b22316b086f463adff4f37a3c8093
Rishib#321 <rishitab2209@gmail.com>
2025-02-04 09:55:26 +0530

    spring aop pointcut-declarations, ordering aspects

--- 03-spring-boot-aop-Ordering-Aspects/src/main/java/com/spring/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 03-spring-boot-aop-Ordering-Aspects/src/main/java/com/spring/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -3,10 +3,12 @@ package com.spring.aopdemo.aspect;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(2)
 public class MyDemoLoggingAspect {
 
     // this is where we add all of our related advices for logging
