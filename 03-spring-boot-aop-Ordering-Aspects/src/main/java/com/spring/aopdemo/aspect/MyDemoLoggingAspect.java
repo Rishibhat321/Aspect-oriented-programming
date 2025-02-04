b842c24ff7538988c43026e2d24bc9b5951635be
Rishib#321 <rishitab2209@gmail.com>
2025-02-04 10:14:30 +0530

    spring aop pointcut-declarations, ordering aspects

--- 03-spring-boot-aop-Ordering-Aspects/src/main/java/com/spring/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 03-spring-boot-aop-Ordering-Aspects/src/main/java/com/spring/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,5 +1,6 @@
 package com.spring.aopdemo.aspect;
 
+import com.spring.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -33,6 +34,23 @@ public class MyDemoLoggingAspect {
         System.out.println("Method: " + methodSignature);
 
         // display method arguments
+
+        // get args
+        Object[] args = theJoinPoint.getArgs();
+
+        // loop through args
+        for(Object tempArg : args) {
+            System.out.println(tempArg);
+
+            if(tempArg instanceof Account) {
+
+                // downcast and print Account specific stuff
+                Account theAccount = (Account) tempArg;
+
+                System.out.println("account name: " + theAccount.getName());
+                System.out.println("account level: " + theAccount.getLevel());
+            }
+        }
 
     }
 
