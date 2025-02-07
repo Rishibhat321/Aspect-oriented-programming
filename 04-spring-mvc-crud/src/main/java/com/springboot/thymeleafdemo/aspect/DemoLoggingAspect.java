9c7f00af2ec817504133d1a0bf87f52fbafb3588
Rishib#321 <rishitab2209@gmail.com>
2025-02-07 21:07:28 +0530

    spring-aop-mvc-crud

--- 04-spring-mvc-crud/src/main/java/com/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
+++ 04-spring-mvc-crud/src/main/java/com/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
@@ -1,6 +1,7 @@
 package com.springboot.thymeleafdemo.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -47,6 +48,21 @@ public class DemoLoggingAspect {
         for(Object tempArg : args) {
             myLogger.info("==> argument: " + tempArg);
         }
+
+    }
+
+    // add @AfterReturning advice
+    @AfterReturning(
+            pointcut = "forAppFlow()",
+            returning = "theRsult")
+    public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+
+        // display method we are returning from
+        String theMethod = theJoinPoint.getSignature().toShortString();
+        myLogger.info("==> in @AfterReturning: calling method: " + theMethod);
+
+        // display data returned
+        myLogger.info("==> result: " + theResult);
 
     }
 }
